a863c5514694fe91b5343d9a5d54477ac1e0d9a6
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-08-20 21:02:23 +0300

    View, Update, Delete

--- src/AppBundle/Controller/TodoController.php
+++ src/AppBundle/Controller/TodoController.php
@@ -85,8 +85,64 @@ class TodoController extends Controller
     public function editAction($id, Request $request)
     {
     	
+        $todo = $this->getDoctrine()
+            ->getRepository('AppBundle:Todo')
+            ->find($id);
+
+            $now = new\DateTime('now');
+
+                $todo->setName($todo->getName());
+                $todo->setCategory($todo->getCategory());
+                $todo->setDescription($todo->getDescription());
+                $todo->setPriority($todo->getPriority());
+                $todo->setDueDate($todo->getDueDate());
+                $todo->setCreateDate($now);
+
+            $form = $this->createFormBuilder($todo)
+            ->add('name', TextType::class, array('attr'=> array('class'=>'form-control', 'style'=>'margin-bottom:15px')))
+            ->add('category', TextType::class, array('attr'=> array('class'=>'form-control', 'style'=>'margin-bottom:15px')))
+            ->add('description', TextareaType::class, array('attr'=> array('class'=>'form-control', 'style'=>'margin-bottom:15px')))
+            ->add('priority', ChoiceType::class, array('choices'=>array('Low'=>'Low', 'Normal'=>'Normal', 'High'=>'High'), 'attr'=> array('class'=>'form-control', 'style'=>'margin-bottom:15px')))
+            ->add('due_date', DateTimeType::class, array('attr'=> array('class'=>'formcontrol', 'style'=>'margin-bottom:15px')))
+            ->add('save', SubmitType::class, array('label'=>'Update Todo', 'attr'=> array('class'=>'btn btn-primary', 'style'=>'margin-bottom:15px')))
+            ->getForm();
+
+            $form->handleRequest($request);
+            if($form->isSubmitted() && $form->isValid()){
+                $name = $form['name']->getData();
+                $category = $form['category']->getData();
+                $description = $form['description']->getData();
+                $priority = $form['priority']->getData();
+                $due_date = $form['due_date']->getData();
+
+                $now = new\DateTime('now');
+
+                $em = $this->getDoctrine()->getManager();
+                $todo = $em->getRepository('AppBundle:Todo')->find($id);
+
+                $todo->setName($name);
+                $todo->setCategory($category);
+                $todo->setDescription($description);
+                $todo->setPriority($priority);
+                $todo->setDueDate($due_date);
+                $todo->setCreateDate($now);
+                
+
+                $em->flush();
+
+                $this->addFlash(
+                        'notice',
+                        'Todo Updated'
+                    );
+                return $this->redirectToRoute('todo_list');
+
+            }
+        
         // replace this example code with whatever you need
-        return $this->render('todo/edit.html.twig');
+        return $this->render('todo/edit.html.twig', array(
+                'todo' => $todo,
+                'form' => $form->createView()
+            ));
     }
     /**
      * @Route("/todo/details/{id}", name="todo_details")
@@ -94,7 +150,31 @@ class TodoController extends Controller
     public function detailsAction($id)
     {
     	
+        $todo = $this->getDoctrine()
+            ->getRepository('AppBundle:Todo')
+            ->find($id);
+        
         // replace this example code with whatever you need
-        return $this->render('todo/details.html.twig');
+        return $this->render('todo/details.html.twig', array(
+                'todo' => $todo
+            ));
+    }
+    /**
+     * @Route("/todo/delete/{id}", name="todo_delete")
+     */
+    public function deleteAction($id)
+    {
+        
+        $em = $this->getDoctrine()->getManager();
+        $todo = $em->getRepository('AppBundle:Todo')->find($id);
+
+        $em->remove($todo);
+        $em->flush();
+
+        $this->addFlash(
+                        'notice',
+                        'Todo Removed'
+                    );
+        return $this->redirectToRoute('todo_list');
     }
 }
